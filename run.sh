5fd3c3de3db8c6723485294160066b1c2e9a3695
Lars Boegild Thomsen <lth@netcompartner.com>
2017-11-14 14:27:24 +0800

    Added locals and sudo

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 id -u $USERNAME &>/dev/null || useradd -u $USERID -s "/bin/bash" $USERNAME
 
+usermod -G sudo lth || true
+
 #exec "su - $USERNAME"
-#exec "/bin/bash"
-exec su -c '/bin/bash' - $USERNAME
+exec "/bin/bash"
+#exec su -c '/bin/bash' - $USERNAME
 
 # vim: ts=4 et nowrap autoindent
